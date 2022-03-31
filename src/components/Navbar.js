aabf6d83d7d8f78b49a4492a3ef6b2d77b92b353
mar <marilirulita@gmail.com>
2022-03-30 19:11:26 -0700

    Add toggle effect in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { ImCross } from 'react-icons/im';
 
-const Navbar = () => (
-  <div>
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
     <div>
-      <h1>Movies Collection</h1>
+      <div>
+        <h1>Movies Collection</h1>
+      </div>
+      <div>
+        <button type="submit" aria-label="Hamburguer Menu" onClick={handleToggle}>
+          { navbarOpen ? <GiHamburgerMenu style={{ color: '#fff', width: '40px', height: '40px' }} /> : <ImCross style={{ color: '#7b7b7b', width: '40px', height: '40px' }} /> }
+        </button>
+        <div className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+          <Link to="/" onClick={closeMenu}>Library</Link>
+          <Link to="/watchedMovies" onClick={closeMenu}>Watched Movies</Link>
+          <Link to="/watchlist" onClick={closeMenu}>Watchlist</Link>
+        </div>
+      </div>
     </div>
-    <div>
-      <button type="submit" aria-label="Hamburguer Menu">
-        <GiHamburgerMenu />
-        <ImCross />
-      </button>
-      <Link to="/">Library</Link>
-      <Link to="/watchedMovies">Watched Movies</Link>
-      <Link to="/watchlist">Watchlist</Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
