7f6def09466684ac6b390ca08ed2fea14d1bf4bd
mar <marilirulita@gmail.com>
2022-03-30 18:41:33 -0700

    Add genre select input

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,21 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { BsTrash } from 'react-icons/bs';
 
 const Library = () => {
   const newMovie = {
     title: '',
-    genere: '',
+    genre: '',
     id: '',
   };
   const [state, setState] = useState(newMovie);
   const [library, setNewMovie] = useState([]);
 
+  const selectGenre = (select) => {
+    setState({ title: state.title, genre: select.options[select.selectedIndex].value });
+  };
+
   const submitNewMovie = () => {
-    if (state.title !== '' && state.genere !== '') {
+    if (state.title !== '' && state.genre !== '') {
       state.id = uuidv4();
       setNewMovie([...library, state]);
-      setState({ title: '', genere: '' });
+      setState({ title: '', genre: '' });
     }
+  };
+
+  const deleteMovie = (id) => {
+    setNewMovie((library) => library.filter((movie) => movie.id !== id));
   };
 
   return (
@@ -27,15 +36,22 @@ const Library = () => {
           type="text"
           placeholder="Add new movie"
           value={state.title}
-          onChange={(e) => setState({ title: e.target.value, genere: state.genere })}
+          onChange={(e) => setState({ title: e.target.value.toUpperCase(), genre: state.genre })}
         />
-        <input
-          className="inputs"
-          type="text"
-          placeholder="Select a genere"
-          value={state.genere}
-          onChange={(e) => setState({ title: state.title, genere: e.target.value })}
-        />
+        <label htmlFor="genre">
+          Choose a genre:
+          <select name="genre" id="genre" onClick={(e) => selectGenre(e.target)}>
+            <option value="Action">Action</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Drama">Drama</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+            <option value="Mystery">Mystery</option>
+            <option value="Romance">Romance</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Western">Western</option>
+          </select>
+        </label>
         <button
           type="submit"
           value="add-movie"
@@ -49,7 +65,16 @@ const Library = () => {
         {library.map((movie) => (
           <div key={movie.id}>
             <h3>{movie.title}</h3>
-            <span>{movie.genere}</span>
+            <span>{movie.genre}</span>
+            <button
+              id={movie.id}
+              type="submit"
+              value="delete"
+              onClick={() => deleteMovie(movie.id)}
+              className="delete-button"
+            >
+              <BsTrash />
+            </button>
           </div>
         ))}
       </div>
