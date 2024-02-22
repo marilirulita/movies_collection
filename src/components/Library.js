24ece63515e1d598b51adb59cdafad74d8042ab4
mar bautista <marilirulita@gmail.com>
2024-02-22 14:16:23 -0800

    add css in library js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BsTrash } from 'react-icons/bs';
 import { addNewMovie, removeMovie, movieWatched } from '../redux/movies/movies';
 import { notOnLibrary, onLibrary } from '../redux/wishes/wishes';
 
@@ -45,63 +44,89 @@ const Library = () => {
 
   return (
     <div className="movies-container">
-      <h2>Add New Movie</h2>
-      <div className="add-new-movie">
-        <input
-          className="inputs"
-          type="text"
-          placeholder="Add new movie"
-          value={state.title}
-          onChange={(e) => {
-            setState({ ...state, title: e.target.value.toUpperCase() });
-          }}
-        />
-        <label htmlFor="genre">
-          <select name="genre" id="genre" onClick={(e) => selectGenre(e.target)}>
-            <option value="Action">Action</option>
-            <option value="Comedy">Comedy</option>
-            <option value="Drama">Drama</option>
-            <option value="Fantasy">Fantasy</option>
-            <option value="Horror">Horror</option>
-            <option value="Mystery">Mystery</option>
-            <option value="Romance">Romance</option>
-            <option value="Thriller">Thriller</option>
-            <option value="Western">Western</option>
-          </select>
-        </label>
-        <button
-          type="submit"
-          value="add-movie"
-          onClick={submitNewMovie}
-          className="add-movie-button"
-        >
-          <span>Add Movie</span>
-        </button>
+      
+      <div className="add-book">
+        <h2 className="add-book-title">Add New Movie</h2>
+        <div className='inputs-elements'>
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Add new movie"
+            value={state.title}
+            onChange={(e) => {
+              setState({ ...state, title: e.target.value.toUpperCase() });
+            }}
+          />
+          {/* ver si le agrego la misma clase que el de arriba y ver si agrego otro input para el author */}
+          <label htmlFor="genre">
+            <select name="genre" id="genre" onClick={(e) => selectGenre(e.target)}>
+              <option value="Action">Action</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Drama">Drama</option>
+              <option value="Fantasy">Fantasy</option>
+              <option value="Horror">Horror</option>
+              <option value="Mystery">Mystery</option>
+              <option value="Romance">Romance</option>
+              <option value="Thriller">Thriller</option>
+              <option value="Western">Western</option>
+            </select>
+          </label>
+          <button
+            type="submit"
+            value="add-movie"
+            onClick={submitNewMovie}
+            className="add-book-button"
+          >
+            <span className="ADD-BOOK">Add Movie</span>
+          </button>
+        </div>
       </div>
-      <div className="display-movies">
+      <div className="books-display">
         {library.length === 0 ? (<h3 className="empty-list">Any movie in your library yet</h3>) : library.map((movie) => (
-          <div key={movie.id} className="movie-container">
-            <div>
-              <h3>{movie.title}</h3>
-              <span>{movie.genre}</span>
+          <div key={movie.id} className="Lesson-Panel">
+            <div className='book-info'>
+              <span className="Category">{movie.genre}</span>
+              <span className="Title">{movie.title}</span>
+              <span className="Author">Categoria 'arreglar'</span>
+              <div className="comments-section">
+                <span className="Comments">Comments</span>
+                <span className="Comments">Remove</span>
+                <span className="Comments">Edit</span>
+              </div>
             </div>
-            <div className="deleteWatch">
-              <button
-                className={(movie.watched && 'btnwatched') || (!movie.watched && 'btnNotWatched')}
-                type="submit"
-                onClick={() => watchedMovie(movie.id, movie.watched)}
-              >
-                {(movie.watched && 'Watched Movie') || (!movie.watched && 'Not Watched')}
-              </button>
-              <button
+            <div className="status-section">
+              <div className="percent-section">
+                <div className="circle">
+                  <div className="circle-oval" />
+                </div>
+                <div className="percent-spans">
+                  <span className="-Percent-Complete">45%</span>
+                  <span className="Completed">Completed</span>
+                  {/* este boton lo tengo que meter en el span de arriba?? */}
+                  <button
+                    className={(movie.watched && 'btnwatched') || (!movie.watched && 'btnNotWatched')}
+                    type="submit"
+                    onClick={() => watchedMovie(movie.id, movie.watched)}
+                  >{(movie.watched && 'Watched Movie') || (!movie.watched && 'Not Watched')}
+                  </button>
+                </div>
+              </div>
+
+              <div className="chapter-section">
+                <span className="Current-Chapter">CURRENT CHAPTER</span>
+                <span className="Current-Lesson">Chapter 13</span>
+                <button
                 id={movie.id}
                 type="submit"
                 value="delete"
                 onClick={() => deleteMovie(movie.id, movie.title)}
-                className="delete-button"
-              >
-                <BsTrash />
-              </button>
+                className="Delete-button"
+                >
+                  <span className="Delete-text">DELETE BOOK</span>
+                </button>
+              </div>
+             
+              
             </div>
           </div>
         ))}
